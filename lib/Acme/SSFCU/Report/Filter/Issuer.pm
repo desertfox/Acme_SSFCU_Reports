bd9a45a318f6a5ab9e4552f01f9db2b3242bfcd5
Rummy <cerealbh@gmail.com>
2021-01-02 23:32:55 -0600

    Updated Issuer Filter
    Updated regex substitution to be more aggressive.
    Swapped sorting.

--- lib/Acme/SSFCU/Report/Filter/Issuer.pm
+++ lib/Acme/SSFCU/Report/Filter/Issuer.pm
@@ -13,12 +13,13 @@ sub calculate {
     my $history_iterator = $history->iterator;
     while ( !$history_iterator->is_done() ) {
         my $description = $history_iterator->get_transaction()->description;
-        $description =~ s/\d{2}\/\d{2}\s+?//;   #Remove dates to help condense
+        $description =~ s/\W|\d//g; #remove fluff in an attempt to make consolidation easier. 
+        $description = 'Amazon Marketplace' if $description =~ m/^AMZN.*/;
         $per_descriptions{$description} += $history_iterator->get_transaction()->amount;
         $history_iterator->next;
     }
 
-    my @sorted_desc = sort { $per_descriptions{$b} <=> $per_descriptions{$a} }
+    my @sorted_desc = sort { $per_descriptions{$a} <=> $per_descriptions{$b} }
         keys %per_descriptions;
 
     my @data;
